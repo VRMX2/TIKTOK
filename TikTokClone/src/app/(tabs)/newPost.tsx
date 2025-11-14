413e467785db57ca5649068a7e7f6ae70269a5f2
VRMX2 <lehcengrissi@gmail.com>
2025-11-14 03:00:38 -0700

    new commit

--- TikTokClone/src/app/(tabs)/newPost.tsx
+++ TikTokClone/src/app/(tabs)/newPost.tsx
@@ -1,13 +1,30 @@
-import {View,Text} from 'react-native';
-import {CameraView, CameraType, useCameraPermessions} from "expo-camera";
-import {useState} from 'react';
+import {View, Text, Button,Linking} from 'react-native';
+import {CameraView, CameraType, useCameraPermissions} from "expo-camera";
+import {useState,useEffect} from 'react';
 
 export default function NewPostScreen(){
     const [facing,setFacing] = useState<CameraType>('back');
 	const [permission, requestPermission] = useCameraPermissions();
+
+	useEffect(() => {
+		if(permission && !permission.granted && permission.canAskAgain){
+			requestPermission();
+        }
+    }, [permission])
+
+	if(permission && !permission.granted && !permission.canAskAgain){
+		return (
+			<View>
+				<Text>We need your permission to use the camera</Text>
+                <Button title="Grant Permissions" onPress={()=>Linking.openSettings}/>
+            </View>
+        )
+    }
+
+
     return(
         <View>
-            <Text style={{color="white"}}>New Post Screen</Text>
+			<Text style = {{color: "white"}}>New Post Screen</Text>
         </View>    
 
     )
